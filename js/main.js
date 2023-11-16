63b30c625fcf78d8ce92bcc84beb7010ca59e790
kwenox25 <113370944+kwenox25@users.noreply.github.com>
2023-11-16 13:25:45 -0500

    added spinner

--- js/main.js
+++ js/main.js
@@ -3,8 +3,24 @@
   const model = document.querySelector("#model");
   const hotspots = document.querySelectorAll(".Hotspot");
 
+  const materialsDiv = document.querySelector(".materials-div");
+
   const materialTemplate = document.querySelector("#material-template");
   const materialList = document.querySelector("#material-list");
+
+  const spinner = `<svg version="1.1" id="L9" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+  viewBox="0 0 100 100" enable-background="new 0 0 0 0" xml:space="preserve">
+  <path fill="#333" d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50">
+  <animateTransform
+  attributeName="transform"
+  attributeType="XML"
+  type="rotate"
+  dur="1s"
+  from="0 50 50"
+  to="360 50 50"
+  repeatCount="indefinite" />
+  </path>
+  </svg>`;
 
   //functions
   function modelLoaded() {
@@ -14,6 +30,7 @@
   }
 
   function loadInfoBoxes() {
+    // model.innerHTML = spinner;
     fetch("https://swiftpixel.com/earbud/api/infoboxes")
       .then((response) => response.json())
       .then((infoBoxes) => {
@@ -29,6 +46,7 @@
           const imgElement = document.createElement("img");
           imgElement.src = `images/${infoBox.thumbnail}`;
 
+          // model.innerHTML = "";
           selected.appendChild(imgElement);
           selected.appendChild(titleElement);
           selected.appendChild(textElement);
@@ -39,6 +57,7 @@
   loadInfoBoxes();
 
   function loadMaterialInfo() {
+    // materialsDiv.innerHTML = spinner;
     fetch("https://swiftpixel.com/earbud/api/materials")
       .then((response) => response.json())
       .then((materialListData) => {
@@ -58,9 +77,10 @@
           materialDescription.textContent = material.description;
 
           //append the populated template to the list.
-
           materialList.appendChild(clone);
         });
+        // materialsDiv.innerHTML = "";
+        // materialList.appendChild(clone);
       })
       .catch((error) => console.error(error)); //catch and report any errors
   }
